2c9b49f5bbbb844360b5f0d061dc5de38f1be284
octavioperrud <octavio.perrud@gmail.com>
2026-09-07 11:53:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,28 +11,28 @@
 // Vamos colocar aqui o link do OneDrive
 // configurado para receber arquivos.
 
-const LINK_ENVIAR_FOTO = "COLE_AQUI_O_LINK_DE_ENVIO";
+// LINK PARA ENVIAR FOTO
 
+const LINK_ENVIAR_FOTO =
+    "COLE_AQUI_O_LINK_DE_ENVIO";
+
+
+// LINK DA PASTA COM AS FOTOS AGUARDANDO
+
+const LINK_FOTOS_AGUARDANDO =
+    "https://1drv.ms/f/c/f203919d62721e53/IgBJxxN6tRZCR6Ei-80o3fdbAUryfiJxQAk-5yxRJelFj0w?e=weFT7p";
 
 
 // LINK DA PASTA COM AS FOTOS APROVADAS
 
 const LINK_FOTOS_APROVADAS =
-    "COLE_AQUI_O_LINK_DA_PASTA_APROVADAS";
-
+    "https://1drv.ms/f/c/f203919d62721e53/IgDBWH5vRVqcTLP_ennS0ThFAbzVWbGUZoULU9zM_F_M4iM?e=bw1XGR";
 
 
 // SENHA DA ÁREA DE VALIDAÇÃO
 
 const SENHA_ADMIN =
     "CASAMENTO2026";
-
-
-const LINK_FOTOS_AGUARDANDO =
-    "https://1drv.ms/f/c/f203919d62721e53/IgBJxxN6tRZCR6Ei-80o3fdbAUryfiJxQAk-5yxRJelFj0w?e=weFT7p";
-
-const LINK_FOTOS_APROVADAS =
-    "https://1drv.ms/f/c/f203919d62721e53/IgDBWH5vRVqcTLP_ennS0ThFAbzVWbGUZoULU9zM_F_M4iM?e=bw1XGR";
 
 
 
